b1f541428ae183e27c5e9e55ec6b79181788a497
loic.huart <loic.huart08@gmail.com>
2024-02-24 17:03:47 +0000

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Fastify, { type FastifyInstance } from 'fastify'
 import { checkEnvVariables } from './configs/configs'
 import dbConnect from './lib/mongoose'
 import routes from './routes'
+import cors from '@fastify/cors'
 
 async function run (): Promise<any> {
   // Check environment variables
@@ -13,12 +14,25 @@ async function run (): Promise<any> {
   const app: FastifyInstance = Fastify({
     logger: true
   })
+  await app.register(cors, {
+    origin: (origin, cb) => {
+      // @ts-expect-error origin is a string
+      const hostname = new URL(origin).hostname
+      if (hostname === 'localhost') {
+        //  Request from localhost will pass
+        cb(null, true)
+        return
+      }
+      // Generate an error on other origins, disabling access
+      cb(new Error('Not allowed'), false)
+    }
+  })
 
   // Routes
   void app.register(routes, { prefix: '/api' })
 
   await app.listen({ port: 8000, host: '0.0.0.0' })
-    .then((address) => console.log(`server listening on ${address}`))
+    .then((address) => { console.log(`server listening on ${address}`) })
     .catch(err => {
       console.log('Error starting server:', err)
       process.exit(1)
