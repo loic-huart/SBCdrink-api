fd88a134f5f24ed380da37d6be540306f5a9a5e5
loic.huart <loic.huart08@gmail.com>
2024-05-01 09:12:54 +0000

    feat(seeder): add machineconfiguration data

--- seeder/seed.sh
+++ seeder/seed.sh
@@ -1,3 +1,4 @@
- mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection ingredients --file ./seeder/data/ingredients.json --jsonArray
- mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection recipes --file ./seeder/data/recipes.json --jsonArray
- mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection orders --file ./seeder/data/orders.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection ingredients --file ./seeder/data/ingredients.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection recipes --file ./seeder/data/recipes.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection orders --file ./seeder/data/orders.json --jsonArray
+mongoimport --uri mongodb://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME --authenticationDatabase=admin --db $DB_NAME --collection machineconfigurations --file ./seeder/data/machineconfigurations.json --jsonArray
